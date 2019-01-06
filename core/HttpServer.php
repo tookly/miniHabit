51c0b922d6e710731ed78080d033d34d762a007c
chengxiaoli <chengxiaoli@qmtv.com>
2019-01-06 22:28:54 +0800

    fix

--- core/HttpServer.php
+++ core/HttpServer.php
@@ -46,7 +46,7 @@ class HttpServer {
     
     public function onWorkerStart()
     {
-        $this->app = (require "/Users/chengxiaoli/Documents/wcworkspace/cat/index.php");
+        $this->app = (require  dirname(__FILE__) . '/../index.php');
     }
     
     public function onConnect()
