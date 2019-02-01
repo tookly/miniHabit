5910a912767cfbe7c4a640cfb6e98af62060b372
chengxiaoli <chengxiaoli@qmtv.com>
2019-02-01 15:29:40 +0800

    mv apiTest ro controllerTest

--- tests/controllerTest/TargetControllerTest.php
+++ tests/controllerTest/TargetControllerTest.php
@@ -1,78 +1,101 @@
 <?php
-///**
-// * Created by PhpStorm.
-// * User: chengxiaoli
-// * Date: 2019/1/31
-// * Time: 下午3:04
-// */
-//
-//namespace controllerTest;
-//
-//use controller\TargetController;
-//use PHPUnit\Framework\TestCase;
-//
-//class TargetControllerTest extends TestCase
-//{
-//    public $controller;
-//
-//    public function __construct(string $name = null, array $data = [], string $dataName = '')
-//    {
-//        require_once dirname(__FILE__) . '/../../vendor/autoload.php';
-//        parent::__construct($name, $data, $dataName);
-//        $this->controller = new TargetController();
-//        $this->controller->request = new \Request();
-//        $this->controller->response = new \Response();  // 如何屏蔽掉request、response，只剩逻辑部分呢？
-//    }
-//
-//    public function setPost($query)
-//    {
-//        foreach ($query as $key => $value) {
-//            $this->controller->setPost($key, $value);
-//        }
-//    }
-//
-//    public function setGet($body)
-//    {
-//        foreach ($body as $key => $value) {
-//            $this->controller->setGet($key, $value);
-//        }
-//    }
-//
-//    public function getResponse()
-//    {
-//        return $this->controller->response->data;
-//    }
-//
-//    public function testSet()
-//    {
-//        // 需要入参、出参，校验入参、出参和内部状态变化
-//        $query['target'] = '每天晚上11点前入睡，早上7点前起床';
-//        $query['targetId'] = -1;
-//        $this->setGet($query);
-//        $this->controller->set();
-//        $res = $this->getResponse();
-//        $this->assertTrue($res['code'], Code::SUCCESS());
-//        $this->assertTrue(true);
-//        return 1;
-//    }
-//
-//    /**
-//     * @depends testSet
-//     * @dataProvider mockDataProvider
-//     */
-//    public function testInfo()
-//    {
-//        $args = func_get_args();
-//        print_r($args);
-//        $this->assertTrue(true);
-//    }
-//
-//    public function mockDataProvider()
-//    {
-//        return [
-//            [1,2,3],
-//            [2,2,4],
-//        ];
-//    }
-//
-//}
+/**
+ * Created by PhpStorm.
+ * User: chengxiaoli
+ * Date: 2019/1/31
+ * Time: 下午3:04
+ */
+
+namespace controllerTest;
+
+require_once dirname(__FILE__) . '/../../vendor/autoload.php';
+
+use component\Code;
+use PHPUnit\Framework\TestCase;
+
+class TargetControllerTest extends TestCase
+{
+    public $host;
+    
+    public $connectTimeOut;
+    
+    public $timeOut;
+    
+    public function setUp()
+    {
+        $this->host = 'http://127.0.0.1:9501';
+        $this->connectTimeOut = 3000;
+        $this->timeOut = 5000;
+    }
+    
+    public function testSet()
+    {
+        // 需要入参、出参，校验入参、出参和内部状态变化
+        $query['target'] = '每天写50个字';
+        $query['targetId'] = 1;
+        $res = $this->post('/target/set', $query);
+        $this->assertEquals($res['code'], Code::SUCCESS[0]);
+    }
+    
+    /**
+     * @depends testSet
+     */
+    public function testInfo()
+    {
+        $query['targetId'] = 1;
+        $res = $this->get('/target/info', $query);
+        $this->assertEquals('每天写50个字', $res['data']['target']);
+    }
+    
+    public function tearDown()
+    {
+//        exec("cd /Users/chengxiaoli/Documents/wcworkspace/miniHabit & pm2 stop pm2_local.json");
+    }
+    
+    public function get($path, $data = [])
+    {
+        $url = $this->host . $path;
+        if (!empty($data)) {
+            $query = http_build_query($data);
+            $url = $url . '?' . $query;
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, $this->connectTimeOut);
+        curl_setopt($ch, CURLOPT_TIMEOUT_MS, $this->timeOut);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_ENCODING, '');
+        $body = curl_exec($ch);
+        $no = curl_errno($ch);
+        if ($no !== 0) {
+            $error = curl_error($ch);
+            echo 'get ' . $url . ' error, error no : ' . $no . ', error : ' . $error . PHP_EOL;
+        }
+        curl_close($ch);
+        return $body ? json_decode($body, true) : [];
+    }
+    
+    public function post($path, $data = [], $header = false)
+    {
+        $url = $this->host . $path;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, $this->connectTimeOut);
+        curl_setopt($ch, CURLOPT_TIMEOUT_MS, $this->timeOut);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_ENCODING, '');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HEADER, $header);
+        $body = curl_exec($ch);
+        $no = curl_errno($ch);
+        if ($no !== 0) {
+            $error = curl_error($ch);
+            echo 'post ' . $url . ' error, error no : ' . $no . ', error : ' . $error . PHP_EOL;
+        }
+        curl_close($ch);
+        return $body ? json_decode($body, true) : [];
+    }
+    
+}
